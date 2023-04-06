08344e1ed54972022b9956d928455fcb77d9b634
Ali Girisen <aliriza.girisen@gmail.com>
2023-04-06 03:32:20 +0300

    assignment-3.2

--- assignment/src/18050111007.c
+++ assignment/src/18050111007.c
@@ -19,135 +19,111 @@ int main(int argc, char *argv[]){
 	param2 = atoi(argv[2]);
 	str = argv[3];
 
-	double* recv_buffer = (double*) malloc(param1 * param2 * sizeof(double));
+	
 
     MPI_Init(NULL,NULL);
     MPI_Comm_size(MPI_COMM_WORLD, &comm_sz);
     MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
-	
-	start_time = MPI_Wtime();
 
-	double vector, random_vector;
-	double output [param1][param2]; 
+	int balance = param1 / (comm_sz-1);
+	
+	
+
+	double random_vector[param2]; 
 	double** matrix = (double**) malloc(param1 * sizeof(double*));
+	double output [balance];
+
+	for (int i = 0; i < param1; i++) {
+      	matrix[i] = (double*) malloc(param2 * sizeof(double));}
+
+	
+	if (my_rank == 0){
+
+		srand(18050111007);
+		start_time = MPI_Wtime();
+		// buffer for received messaging X invalid buffer pointer ?
+		//double* recv_buffer = (double*) malloc(param1 * param2 * sizeof(double));
+
+		// received and converted to 2d array
 	
 
-	if (my_rank == 0){
 		for (int i = 0; i < param1; i++) {
-      		matrix[i] = (double*) malloc(param2 * sizeof(double));
+			
       		for (int j = 0; j < param2; j++) {
-        		matrix[i][j] = recv_buffer[i*param2 + j];
+				random = randomGenerator();
+        		matrix[i][j] = random;
       		}
     	}
-		for (int i  = 0; i< param1*param2 ;i++){
-			printf("%d.recv element : %ls\n",i,recv_buffer[i]);
-		}
+		for (int j = 0; j < param2; j++) {
+			random_vector[j] = rand() % 100 + 1; 
+      	}
 
-		//MPI_Recv(recv_buffer, param1*param2, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+		/*
+		my_rank = my_rank + 1;
+		while(my_rank < comm_sz){
+			MPI_Recv(output, param1*param2, MPI_DOUBLE, my_rank, 99, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+			MPI_Send(random_vector, param2, MPI_DOUBLE, my_rank, 98, MPI_COMM_WORLD);
+			MPI_Send(matrix, param1*param2, MPI_DOUBLE, my_rank, 101, MPI_COMM_WORLD);
+
+			for (int i = 0; i < param1; i++ ){
+				for (int j = 0; j < param2 ; j++){
+					result_matrix[i][j] = recv_buffer[i*param2 + j];
+				}
+			}
+
+
+			my_rank = my_rank + 1;*/
 		
-	}
+		for ( int i = 1 ; i < comm_sz ; i ++){
+			
+			MPI_Send(random_vector, param2, MPI_DOUBLE, i, 98, MPI_COMM_WORLD);
+			MPI_Send(matrix, param1*param2, MPI_DOUBLE, i, 101, MPI_COMM_WORLD);
+			MPI_Recv(output, balance, MPI_DOUBLE, i, 99, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+		}
+		end_time = MPI_Wtime();
+		elapsed_time = end_time - start_time;
+	
+	
+		printf("Elapsed time is %f seconds for parallel mxv with %d \n",elapsed_time,my_rank);
+		file = fopen(str,"w");  
+		for (int i = 0; i < param1*param2; i++) {
+			fprintf(file, "%0.2f \n", output[i]);
+    	}
+		fclose(file);
+	}		
+
+	
 	
 	else{
 
-		// if else conditions for load balancing
-		if (my_rank == 1){
-			double* send_buffer_p1 = (double*) malloc(((param1 * param2)/comm_sz) * sizeof(double));
-			MPI_Allreduce(send_buffer_p1, recv_buffer, (param1*param2)/4, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
+		MPI_Recv(&matrix[0][0], param1 * param2, MPI_DOUBLE, 0, 101, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+		MPI_Recv(&random_vector[0], param2, MPI_DOUBLE, 0, 98, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+		int rank_range = (my_rank-1) * balance;
+		int rank_endrange = my_rank * balance;
+		double result = 0.0;
+		double sum = 0.0;
+		int count = 0;
+
+			//double* send_buffer_p1 = (double*) malloc(param1 * param2* sizeof(double));
 
 
-			//for (int i = 0; i < ((my_rank*(param1*param2))/comm_sz); i++) 
-			for (int i=0; i < ((param1*param2)/comm_sz); i++){
-				srand(8+i);
-				random_vector = randomGenerator();
-				printf("1");
-					srand(18050111007+i);
-					random = randomGenerator();
-					send_buffer_p1[i] += random * random_vector;
-    			
-  			}
-
-			free(send_buffer_p1);
-		}else if(my_rank == 2){
-			double* send_buffer_p2 = (double*) malloc(((param1 * param2)/comm_sz) * sizeof(double));
-			MPI_Allreduce(send_buffer_p2, recv_buffer, (param1*param2)/4, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
-
-			//for (int i = (((my_rank-1)*(param1*param2))/comm_sz); i < ((my_rank*(param1*param2))/comm_sz); i++) 
-			for (int i=0; i < ((param1*param2)/comm_sz); i++){
-				srand(8+i);
-				random_vector = randomGenerator();
-    			for (int j = 0; j < param2; j++) {
-					srand(18050111007+j);
-					random = randomGenerator();
-					send_buffer_p2[i] += random * random_vector;
-    			}
-  			}
-			free(send_buffer_p2);
-		}else if(my_rank == 3){
-			double* send_buffer_p3 = (double*) malloc(((param1 * param2)/comm_sz) * sizeof(double));
-			MPI_Allreduce(send_buffer_p3, recv_buffer, (param1*param2)/4, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
-
-			//for (int i = (((my_rank-1)*(param1*param2))/comm_sz); i < ((my_rank*(param1*param2))/comm_sz); i++)
-			for (int i=0; i < ((param1*param2)/comm_sz); i++) {
-				srand(8+i);
-				random_vector = randomGenerator();
-    			for (int j = 0; j < param2; j++) {
-					srand(18050111007+j);
-					random = randomGenerator();
-					send_buffer_p3[i] += random * random_vector;
-    			}
-  			}
-			free(send_buffer_p3);
-		}else if (my_rank == 4){
-			double* send_buffer_p4 = (double*) malloc(((param1 * param2)/comm_sz) * sizeof(double));
-			MPI_Allreduce(send_buffer_p4, recv_buffer, (param1*param2)/4, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
-
-			//for (int i = (((my_rank-1)*(param1*param2))/comm_sz); i < ((my_rank*(param1*param2))/comm_sz); i++) 
-			for (int i=0; i < ((param1*param2)/comm_sz); i++){
-				srand(8+i);
-				random_vector = randomGenerator();
-				printf("4");
-    
-				srand(18050111007+i);
-				random = randomGenerator();
-				send_buffer_p4[i] += random * random_vector;
-    			
-  			}
-			free(send_buffer_p4);
-		}else if(my_rank == 5){
-			double* send_buffer_p5 = (double*) malloc(((param1 * param2)/comm_sz) * sizeof(double));
-			MPI_Allreduce(send_buffer_p5, recv_buffer, (param1*param2)/4, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
-
-			//for (int i = (((my_rank-1)*(param1*param2))/comm_sz); i < ((my_rank*(param1*param2))/comm_sz); i++)
-			for (int i=0; i < ((param1*param2)/comm_sz); i++) {
-				srand(8+i);
-				random_vector = randomGenerator();
-    			for (int j = 0; j < param2; j++) {
-					srand(18050111007+j);
-					random = randomGenerator();
-					send_buffer_p5[i] += random * random_vector;
-    			}
-  			}
-			free(send_buffer_p5);
-		}
-
-
+			
+			for(int i = rank_range ; i <  rank_endrange; i++){
+				for(int j = 0; j < param2; j++){
+					sum = matrix[i][j] * random_vector[j];
+					result = result + sum;
+				}
+				output[count] = sum;
+				count = count + 1;
+				result = 0.0;
+			}
+		MPI_Send(output, balance, MPI_DOUBLE, 0, 99, MPI_COMM_WORLD);
 	}
-	free(recv_buffer);
-
-	end_time = MPI_Wtime();
-	MPI_Finalize();
-	elapsed_time = end_time - start_time;
-	printf("Elapsed time is %f seconds for parallel mxv with %d \n",elapsed_time,my_rank);
-
-	file = fopen(str,"w");  
-	for (int i = 0; i < param1; i++) {
-        for (int j = 0; j < param2; j++) {
-			fprintf(file, "%0.2f ", matrix[i][j]);
-        }
-		fprintf(file,"\n");
-    }
+	
 	free(matrix);
-	fclose(str);
+	MPI_Finalize();
 	
 	return(0);
 }
